f42cd3481d9a84010f6db7186467be3f9e376b3d
tfjoek <125345194+tfjoek@users.noreply.github.com>
2024-11-01 12:04:04 +0200

    Create moduuli11.py

---
+++ python_tehtavat/ohjelmistoteema/moduuli11.py
@@ -0,0 +1,38 @@
+class Julkaisu:
+    def __init__(self, nimi):
+        self.nimi = nimi
+
+    def tulosta_tiedot(self):
+        print(f'Julkaisun nimi: {self.nimi}')
+
+
+class Kirja(Julkaisu):
+    def __init__(self, nimi, kirjoittaja, sivumaara):
+        super().__init__(nimi)
+        self.kirjoittaja = kirjoittaja
+        self.sivumaara = sivumaara
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f'Kirjoittaja: {self.kirjoittaja}')
+        print(f'Sivumäärä: {self.sivumaara}')
+
+
+class Lehti(Julkaisu):
+    def __init__(self, nimi, paatoimittaja):
+        super().__init__(nimi)
+        self.paatoimittaja = paatoimittaja
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f'Päätoimittaja: {self.paatoimittaja}')
+
+
+# Pääohjelma
+if __name__ == "__main__":
+    julkaisu1 = Lehti("Aku Ankka", "Aki Hyyppä")
+    julkaisu2 = Kirja("Hytti n:o 6", "Rosa Liksom", 200)
+
+    julkaisu1.tulosta_tiedot()
+    print()  # Tyhjää riviä erottamaan julkaisut
+    julkaisu2.tulosta_tiedot()
